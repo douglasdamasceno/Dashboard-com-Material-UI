ba49573efc0ceb9712c569fe6028c663663e002e
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-06-01 20:02:12 -0300

    feat: add toolbar

--- src/shared/layouts/LayoutBasePage.tsx
+++ src/shared/layouts/LayoutBasePage.tsx
@@ -11,20 +11,26 @@ interface ILayoutBasePageProps {
 export const LayoutBasePage: React.FC<ILayoutBasePageProps> = ({children,title,toolbar}) => {
     
     const isSmDown = useMediaQuery((theme:Theme)=>theme.breakpoints.down('sm'));
+    const isMdDown = useMediaQuery((theme:Theme)=>theme.breakpoints.down('md'));
     const theme = useTheme();
 
     const {toggleDrawerOpen} = useDrawerContext();
 
     return(
     <Box height='100%' display='flex' flexDirection='column' gap={1}>
-        <Box display='flex' alignItems='center'padding={1} height={theme.spacing(12)}>
+        <Box display='flex' alignItems='center'padding={1} gap={1} height={theme.spacing(isSmDown? 6: isMdDown ? 8: 12)}>
         {
             isSmDown &&
             <IconButton onClick={toggleDrawerOpen}>
                 <Icon>menu</Icon>
             </IconButton>
         }
-        <Typography variant="h5">
+        <Typography 
+            overflow='hidden'
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            variant={isSmDown ? 'h5': isMdDown? 'h4':'h3'}
+        >
             {title}
         </Typography>
         </Box>
@@ -35,7 +41,7 @@ export const LayoutBasePage: React.FC<ILayoutBasePageProps> = ({children,title,t
                 </Box>
             )
         }
-        <Box>
+        <Box flex={1} overflow="auto">
             {children}
         </Box>
     </Box>
