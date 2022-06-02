1bc1cc3a2e0baf7469fed01ba125a4ea59d8fb1d
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-06-02 18:09:22 -0300

    feat: add variavel de ambiente

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,14 @@
-export {};
+export const Environment = {
+    /**
+     * Define a quantidade de linhas a ser carregada por padrão na listagem.
+     */
+    LIMITE_DE_LINHAS :10,
+    /**
+     * Texto exibido quando não há dados para exibir na listagem.
+     */
+    LISTAGEM_VAZIA:'Nenhum registro encontrado',
+    /**
+     * Url base de consulta de dados da aplicação.
+     */
+    URL_BASE:'http://localhost:3333',
+};
--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,8 +1,9 @@
+import { Environment } from './../../../environment';
 import axios from 'axios';
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: Environment.URL_BASE,
 });
 
 Api.interceptors.response.use(
